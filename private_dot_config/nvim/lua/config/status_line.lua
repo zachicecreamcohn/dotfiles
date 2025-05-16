e1dd2eceb63ddea242256f46f9b3185820e40d73
Zach Cohn <zcohn@wustl.edu>
2025-05-16 14:52:19 -0400

    updated statusline to show pwd

--- private_dot_config/nvim/lua/config/status_line.lua
+++ private_dot_config/nvim/lua/config/status_line.lua
@@ -1,15 +1,14 @@
-local function update_tmux_pwd()
+vim.opt.laststatus = 0
+vim.opt.showtabline = 0
+
+function _G.update_tmux_pwd()
 	local pwd = vim.fn.getcwd()
 	pwd = pwd:gsub("'", "'\\''")
 	os.execute("tmux set-environment NVIM_PWD '" .. pwd .. "' && tmux refresh-client -S")
 end
 
 vim.api.nvim_create_autocmd("DirChanged", {
-	callback = update_tmux_pwd,
+	callback = _G.update_tmux_pwd,
 })
 
-update_tmux_pwd()
-
-vim.opt.laststatus = 0
-vim.opt.cmdheight = 0
-vim.opt.showtabline = 0
+_G.update_tmux_pwd()
